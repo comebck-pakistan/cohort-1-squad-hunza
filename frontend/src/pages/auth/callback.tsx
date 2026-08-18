6ed06011e24187dfc2d8b88f8962df7ac75cc2f7
Huzaifa828 <huzaifaimranalam@gmail.com>
2026-08-19 03:24:13 +0500

    auth

--- frontend/src/pages/auth/callback.tsx
+++ frontend/src/pages/auth/callback.tsx
@@ -1,23 +1,23 @@
-// import { useEffect } from 'react';
-// import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-// export default function AuthCallback() {
-//   const router = useRouter();
+export default function AuthCallback() {
+  const router = useRouter();
 
-//   useEffect(() => {
-//     const hash = window.location.hash.substring(1);
-//     const params = new URLSearchParams(hash);
-//     const accessToken = params.get('access_token');
-//     const refreshToken = params.get('refresh_token');
+  useEffect(() => {
+    const hash = window.location.hash.substring(1);
+    const params = new URLSearchParams(hash);
+    const accessToken = params.get('access_token');
+    const refreshToken = params.get('refresh_token');
 
-//     if (accessToken) {
-//       localStorage.setItem('access_token', accessToken);
-//       localStorage.setItem('refresh_token', refreshToken || '');
-//       router.push('/dashboard');
-//     } else {
-//       router.push('/login');
-//     }
-//   }, []);
+    if (accessToken) {
+      localStorage.setItem('access_token', accessToken);
+      localStorage.setItem('refresh_token', refreshToken || '');
+      router.push('/dashboard');
+    } else {
+      router.push('/login');
+    }
+  }, []);
 
-//   return <div>Signing you in...</div>;
-// }
+  return <div>Signing you in...</div>;
+}
