25de64c1fee410fb6af434b0b9c7a7c18deb946d
Huzaifa828 <huzaifaimranalam@gmail.com>
2026-08-18 15:32:04 +0500

    thats the model

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
 
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"
-GROQ_MODEL = "GPT OSS 20B"
+GROQ_MODEL = "openai/gpt-oss-20b"
 GPT_MODEL=""
 # similarity threshold for duplicate question detection
 # if a question matches the job posting above this score, flag as duplicate
